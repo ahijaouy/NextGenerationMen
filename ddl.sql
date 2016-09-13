5635d070d64ced547f41ddb8554151ed47847774
Oberon <ahijaouy@gatech.edu>
2016-09-12 22:26:39 -0400

    DDL. End of 9/12 Meeting

--- ddl.sql
+++ ddl.sql
@@ -1,9 +1,13 @@
-CREATE TABLE Student (DATE dob);
+CREATE TABLE Student (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, dob DATE NOT NULL, startdate DATE NOT NULL, PRIMARY KEY(id));
 
-CREATE TABLE Quiz();
+CREATE TABLE PreRec(id INTEGER NOT NULL, ms_suspensions INTEGER, hs_suspensions INTEGER, hs_absenses INTEGER, PRIMARY KEY(id));
 
-CREATE TABLE BasicInfo();
-
-CREATE TABLE PreRec();
-
-CREATE TABLE CurrentRec();
+CREATE TABLE CurrentRec
+    (
+        id INTEGER NOT NULL,
+        gpa DECIMAL(5,2) NOT NULL,
+        semester INTEGER NOT NULL,
+        credits_earned INTEGER NOT NULL,
+        total_credits INTEGER NOT NULL,
+        PRIMARY KEY(id)
+    );
