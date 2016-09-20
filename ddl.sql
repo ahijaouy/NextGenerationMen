d90d49abb34aa4299d7fa260e98a2ff6b93b6034
Prateek Shah <pcshah1996@gmail.com>
2016-09-19 21:21:51 -0400

    formatting

--- ddl.sql
+++ ddl.sql
@@ -1,4 +1,11 @@
-CREATE TABLE Student (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, dob DATE NOT NULL, startdate DATE NOT NULL, PRIMARY KEY(id));
+CREATE TABLE Student
+	(
+		id INTEGER NOT NULL, 
+		name VARCHAR(255) NOT NULL, 
+		dob DATE NOT NULL, 
+		startdate DATE NOT NULL, 
+		PRIMARY KEY(id)
+	);
 
 CREATE TABLE PreRec(id INTEGER NOT NULL, ms_suspensions INTEGER, hs_suspensions INTEGER, hs_absenses INTEGER, PRIMARY KEY(id));
 
