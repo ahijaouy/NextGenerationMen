36a1704d45a3a5c7168debb81f3d772f2a5ff80b
Oberon <ahijaouy@gatech.edu>
2016-10-24 21:22:59 -0400

    Updated UI

--- oldUIWork/myapp.js
+++ oldUIWork/myapp.js
@@ -10,6 +10,22 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'Hey',
                         message: 'Hello there!'});
 });
+app.get('/students', function (req, res) {
+  res.render('students', { title: 'Hey',
+                        message: 'Hello there!'});
+});
+app.get('/schools', function (req, res) {
+  res.render('schools', { title: 'Hey',
+                        message: 'Hello there!'});
+});
+app.get('/staff', function (req, res) {
+  res.render('staff', { title: 'Hey',
+                        message: 'Hello there!'});
+});
+app.get('/survey', function (req, res) {
+  res.render('survey', { title: 'Hey',
+                        message: 'Hello there!'});
+});
 app.listen(3000, function() {
     console.log('example app listening on port 3000!');
 });
