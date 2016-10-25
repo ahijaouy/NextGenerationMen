fabaf2467fe6920698a6a39e69cf1aef0101c808
pdorsch6 <pdorsch6@gatech.edu>
2016-10-24 22:25:26 -0400

    Changed ddl

--- utilities/ddl.sql
+++ utilities/ddl.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Student
 	(
 	    id INTEGER NOT NULL,
-	    name VARCHAR(255) NOT NULL,
+	    first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
 	    dob DATE NOT NULL,
         startdate DATE NOT NULL,
         phonenum INTEGER NOT NULL,
@@ -19,10 +20,12 @@ CREATE TABLE PreRec
     (
         id INTEGER NOT NULL AUTO_INCREMENT,
         pr_sid INTEGER NOT NULL,
+        prev_school VARCHAR(255) NOT NULL,
         ms_suspensions INTEGER,
         hs_suspensions INTEGER,
         hs_absenses INTEGER,
         CONSTRAINT fk_prerec FOREIGN KEY(pr_sid) REFERENCES Student(id),
+        CONSTRAINT fk_prerecschool FOREIGN KEY(prev_school) REFERENCES School(school_id),
         PRIMARY KEY(id)
     );
 
@@ -30,11 +33,13 @@ CREATE TABLE CurrentRec
     (
         id INTEGER NOT NULL AUTO_INCREMENT,
         cr_sid INTEGER NOT NULL,
+        curr_school VARCHAR(255) NOT NULL,
         gpa DECIMAL(5,2) NOT NULL,
         semester INTEGER NOT NULL,
         credits_earned INTEGER NOT NULL,
         total_credits INTEGER NOT NULL,
         CONSTRAINT fk_currec FOREIGN KEY(cr_sid) REFERENCES Student(id),
+        CONSTRAINT fk_currecschool FOREIGN KEY(curr_school) REFERENCES School(school_id),
         PRIMARY KEY(id)
     );
 
@@ -42,9 +47,11 @@ CREATE TABLE SemesterRecord
     (
         id INTEGER NOT NULL AUTO_INCREMENT,
         sr_sid INTEGER NOT NULL,
+        curr_school VARCHAR(255) NOT NULL,
         extra_curr TEXT,
         grades TEXT NOT NULL,
         CONSTRAINT fk_semrec FOREIGN KEY(sr_sid) REFERENCES Student(id),
+        CONSTRAINT fk_semrecschool FOREIGN KEY(curr_school) REFERENCES School(school_id),
         PRIMARY KEY(id)
     );
 
@@ -55,4 +62,13 @@ CREATE TABLE SurveyData
         surveyXML TEXT NOT NULL,
         CONSTRAINT fk_survey FOREIGN KEY(sd_sid) REFERENCES Student(id),
         PRIMARY KEY(id)
+    );
+
+CREATE TABLE School
+    (
+        school_id INTEGER NOT NULL AUTO_INCREMENT,
+        school_name VARCHAR(255) NOT NULL,
+        school_address VARCHAR(255),
+        school_phone INTEGER,
+        PRIMARY KEY(school_id)
     );
