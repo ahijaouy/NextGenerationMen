593d7aaaa29bfa327077b202d05433a75af5492b
pdorsch6 <pdorsch6@gatech.edu>
2016-11-10 18:15:09 -0500

    changed ddl.sql

--- utilities/ddl.sql
+++ utilities/ddl.sql
@@ -11,32 +11,41 @@ CREATE TABLE users
 
 CREATE TABLE Student
 	(
-	    id INTEGER NOT NULL,
+	    id INTEGER NOT NULL AUTO_INCREMENT,
 	    first_name VARCHAR(255) NOT NULL,
         last_name VARCHAR(255) NOT NULL,
 	    dob DATE NOT NULL,
         startdate DATE NOT NULL,
-        phonenum INTEGER NOT NULL,
+        phonenum VARCHAR(20) NOT NULL,
         email VARCHAR(255) NOT NULL,
         parentone_name VARCHAR(255) NOT NULL,
-        parentone_num INTEGER NOT NULL,
+        parentone_num VARCHAR(20) NOT NULL,
         parentone_email VARCHAR(255) NOT NULL,
         parenttwo_name VARCHAR(255),
-        parenttwo_num INTEGER,
+        parenttwo_num VARCHAR(20),
         parenttwo_email VARCHAR(255),
         PRIMARY KEY(id)
 	);
-
+    
+CREATE TABLE School
+    (
+        school_id INTEGER NOT NULL AUTO_INCREMENT,
+        school_name VARCHAR(255) NOT NULL,
+        school_address VARCHAR(255),
+        school_phone VARCHAR(20),
+        PRIMARY KEY(school_id)
+    );
+    
 CREATE TABLE PreRec
     (
         id INTEGER NOT NULL AUTO_INCREMENT,
         pr_sid INTEGER NOT NULL,
-        prev_school VARCHAR(255) NOT NULL,
+        prev_school_id INTEGER NOT NULL,
         ms_suspensions INTEGER,
         hs_suspensions INTEGER,
-        hs_absenses INTEGER,
+        hs_absences INTEGER,
         CONSTRAINT fk_prerec FOREIGN KEY(pr_sid) REFERENCES Student(id),
-        CONSTRAINT fk_prerecschool FOREIGN KEY(prev_school) REFERENCES School(school_id),
+        CONSTRAINT fk_prerecschool FOREIGN KEY(prev_school_id) REFERENCES School(school_id),
         PRIMARY KEY(id)
     );
 
@@ -44,13 +53,14 @@ CREATE TABLE CurrentRec
     (
         id INTEGER NOT NULL AUTO_INCREMENT,
         cr_sid INTEGER NOT NULL,
-        curr_school VARCHAR(255) NOT NULL,
+        curr_school_id INTEGER NOT NULL,
         gpa DECIMAL(5,2) NOT NULL,
         semester INTEGER NOT NULL,
         credits_earned INTEGER NOT NULL,
         total_credits INTEGER NOT NULL,
+        grade_level INTEGER NOT NULL,
         CONSTRAINT fk_currec FOREIGN KEY(cr_sid) REFERENCES Student(id),
-        CONSTRAINT fk_currecschool FOREIGN KEY(curr_school) REFERENCES School(school_id),
+        CONSTRAINT fk_currecschool FOREIGN KEY(curr_school_id) REFERENCES School(school_id),
         PRIMARY KEY(id)
     );
 
@@ -58,11 +68,11 @@ CREATE TABLE SemesterRecord
     (
         id INTEGER NOT NULL AUTO_INCREMENT,
         sr_sid INTEGER NOT NULL,
-        curr_school VARCHAR(255) NOT NULL,
+        curr_school_id INTEGER NOT NULL,
         extra_curr TEXT,
         grades TEXT NOT NULL,
         CONSTRAINT fk_semrec FOREIGN KEY(sr_sid) REFERENCES Student(id),
-        CONSTRAINT fk_semrecschool FOREIGN KEY(curr_school) REFERENCES School(school_id),
+        CONSTRAINT fk_semrecschool FOREIGN KEY(curr_school_id) REFERENCES School(school_id),
         PRIMARY KEY(id)
     );
 
@@ -75,11 +85,4 @@ CREATE TABLE SurveyData
         PRIMARY KEY(id)
     );
 
-CREATE TABLE School
-    (
-        school_id INTEGER NOT NULL AUTO_INCREMENT,
-        school_name VARCHAR(255) NOT NULL,
-        school_address VARCHAR(255),
-        school_phone INTEGER,
-        PRIMARY KEY(school_id)
-    );
+
