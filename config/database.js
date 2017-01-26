2a1fcd9007ba2b52e0083a444cf525f9c5c2d463
Andre Hijaouy <hijaouya@gmail.com>
2017-01-26 14:40:27 -0500

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    'connection': {
+        'host': 'ec2-35-161-110-16.us-west-2.compute.amazonaws.com',
+        'user': 'epic',
+        'password': 'intentions'
+    },
+	'database': 'ngm',
+    'users_table': 'USERS'
+};
+
+//Testing Git ignore 2
