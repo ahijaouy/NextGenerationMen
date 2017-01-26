79c790189dd86bf9181b21809670c70f2b2ea004
Oberon <ahijaouy@gatech.edu>
2017-01-26 01:08:27 -0500

    added basic Create for Students from POST

--- server.js
+++ server.js
@@ -71,6 +71,20 @@ app.get('/addStudent.html', function(req, res) {
 	res.render('addStudent');
 });
 
+app.post('/addStudent.html', function(req, res) {
+	//res.render('addStudent');
+  res.send('POST request recieved');
+  console.log(req.body.dob);
+  stmt = 'INSERT INTO Student(first_name, last_name,dob,startdate,phonenum,email,parentone_name,parentone_num,parentone_email,parenttwo_name,parenttwo_num,parenttwo_email, cohort, school) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
+  
+  connection.query(stmt,['first_name', 'last_name',new Date(req.body.dob), new Date(req.body.dob),'phonenum','email','parentone_name','parentone_num','parentone_email','parenttwo_name','parenttwo_num','parenttwo_email', 'cohort', 'school'], function(err, rows){ 
+    console.log(err);
+  });
+  
+  console.log('yes!');
+  
+});
+
 app.get('/addSchool.html', function(req, res) {
 	res.render('addSchool');
 });
