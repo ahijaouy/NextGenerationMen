a022cdb1fa2ee76e89983ff18ece6348c083c491
Oberon <ahijaouy@gatech.edu>
2017-01-26 14:29:16 -0500

    set up database.js for universal use

--- config/database.js
+++ config/database.js
@@ -1,14 +1,8 @@
 module.exports = {
     'connection': {
-<<<<<<< HEAD
         'host': 'ec2-35-161-110-16.us-west-2.compute.amazonaws.com',
-        'user': 'andre',
-        'password': 'hijaouy'
-=======
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'password'
->>>>>>> 872ed7dcc2de89a197b2cc4eacfd7c27c2e4081e
+        'user': 'epic',
+        'password': 'intentions'
     },
 	'database': 'ngm',
     'users_table': 'USERS'
