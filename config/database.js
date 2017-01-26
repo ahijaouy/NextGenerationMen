8437b57ea301c1f9fe796b2514630b3d4af62b65
Oberon <ahijaouy@gatech.edu>
2017-01-26 11:43:15 -0500

    test

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': 'ec2-35-161-110-16.us-west-2.compute.amazonaws.com',
+        'user': 'andre',
+        'password': 'hijaouy'
     },
 	'database': 'ngm',
     'users_table': 'USERS'
