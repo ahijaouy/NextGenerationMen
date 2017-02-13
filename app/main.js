02424b17fc52e6e91dd489fc73482bd511a29d16
Oberon <ahijaouy@gatech.edu>
2017-02-13 11:23:26 -0500

    change autho back

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ module.exports = function(app, passport) {
   var env = {
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-    AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://admin.ngmatlanta.org/callback'
+    AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost/callback'
   };
   
 
