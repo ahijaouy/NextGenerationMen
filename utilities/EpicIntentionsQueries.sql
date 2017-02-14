3cd5e8b755d9495e590916f7d9b8b5c4cda56670
Jason Chan <jason27chan@gmail.com>
2017-02-14 16:13:26 -0500

    add first seed scripts in EpicintentionsQueries

--- utilities/EpicIntentionsQueries.sql
+++ utilities/EpicIntentionsQueries.sql
@@ -78,7 +78,59 @@ INSERT INTO SurveyData
 VALUES
 (	'1',
 	'SurveyResults');
+INSERT INTO survey (
+    survey_id,
+    survey_name,
+    date_modified,
+    user_modified
+) VALUES (
+    1,
+    'NGM survey_id',
+    now(),
+    1
+)
 
+INSERT INTO survey_category (
+    survey_category_id,
+    survey_id,
+    survey_category_name,
+    date_modified,
+    user_modified,
+) VALUES (
+    1,
+    (SELECT survey_id from survey where survey_name='Survey'),
+    'Grit',
+    now(),
+    1
+);
+
+INSERT INTO survey_question (
+    survey_question_id,
+    survey_category_id,
+    question,
+    question_negated,
+    date_modified,
+    user_modified
+) VALUES (
+    1,
+    (SELECT survey_category_id from survey_category where survey_category_id=1),
+    'New ideas and projects sometimes distract me from previous ones.',
+    0,
+    now(),
+    1
+)
+
+--INSERT INTO survey_category (
+--     survey_category_id,
+--     survey_id,
+--     survey_category_name,
+--     date_modified,
+--     user_modified)
+-- VALUES(
+--     1,
+--     (SELECT survey_id from survey where survey_name="NGM Survey"),
+--     "Grit",
+--     NOW(),1);
 
 #gets all names, school, gpa, and gradelevel in descending order by last name (A-Z)
 SELECT  St.last_name, St.first_name, Sch.school_name, C.gpa, C.grade_level
