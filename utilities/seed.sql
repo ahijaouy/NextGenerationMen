94aa62cfe20561c719f976b8c00c75d88a653085
Jason Chan <jason27chan@gmail.com>
2017-02-17 16:08:27 -0500

    formatting lines

--- utilities/seed.sql
+++ utilities/seed.sql
@@ -19,4 +19,4 @@ INSERT INTO school (
 )
 
 INSERT INTO school (school_id,school_name,school_address,school_phone,principal_name,principal_phone,date_modified,user_modified) VALUES (2,"School2","321 Sunny Lane","3211231234","Johnathan Smith","3214561234",Now(),1);
-INSERT INTO school (school_id, school_name, school_address, school_phone, principal_name, principal_phone, date_modified, user_modified) VALUES (3, "School3", "213 Sunny Lane","2131231234","Jonathan Smith","2134561234",Now(), 1);
+INSERT INTO school (school_id,school_name,school_address,school_phone,principal_name,principal_phone,date_modified,user_modified) VALUES (3,"School3","213 Sunny Lane","2131231234","Jonathan Smith","2134561234",Now(), 1);
