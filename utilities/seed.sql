13f9291f4c010b510e63c65181fcccb54631a7a7
Jason Chan <jason27chan@gmail.com>
2017-02-19 18:06:22 -0500

    add two more classes of students

--- utilities/seed.sql
+++ utilities/seed.sql
@@ -64,6 +64,16 @@ INSERT INTO cohort (
 )
 
 -- Lindsay	Morrison
+-- Frances	Logan
+-- Dixie	Holland
+-- Wendy	Burns
+-- Marianne	Doyle
+-- Mamie	Lynch
+-- Nellie	Hawkins
+-- Raymond	Bush
+-- Phillip	Farmer
+-- Stuart	Allison
+-- Thomas	Sutton
 -- Jeanne	Obrien
 -- Ray	Massey
 -- Perry	Mccormick
@@ -224,6 +234,280 @@ INSERT INTO cohort (
 ) VALUES (
     95047,
     1,
+    "Lisa",
+    "Hall",
+    2002-3-06,
+    2015-5-17,
+    9598351272,
+    "LHall54@gmail.com",
+    "Ken",
+    "KHall40@gmail.com",
+    6520856241
+    "Sharon",
+    "SHall60@gmail.com"
+    2942960351,
+    6,
+    7,
+    4,
+    3.82,
+    23,
+    Now(),
+    1
+)
+
+-- TODO
+-- School 2
+INSERT INTO cohort (
+    student_id,
+    cohort_id,
+    student_first_name,
+    student_last_name,
+    student_dob,
+    student_start_date,
+    student_phone,
+    student_email,
+    guardian_one_name,
+    guardian_one_email,
+    guardian_one_phone,
+    guardian_two_name,
+    guardian_two_email,
+    guardian_two_phone,
+    middleschool_absences,
+    highschool_absences,
+    highschool_suspensions,
+    total_credits_earned,
+    date_modified,
+    user_modified
+) VALUES (
+    92347,
+    2,
+    "Rick",
+    "Rios",
+    2002-1-31,
+    2015-8-09,
+    5018989452,
+    "RRios23@gmail.com",
+    "Thomas",
+    "TRios66@gmail.com",
+    6529375671
+    "Nellie",
+    "NRios87@gmail.com"
+    9101712902,
+    2,
+    3,
+    2,
+    3.88,
+    29,
+    Now(),
+    1
+)
+
+INSERT INTO cohort (
+    student_id,
+    cohort_id,
+    student_first_name,
+    student_last_name,
+    student_dob,
+    student_start_date,
+    student_phone,
+    student_email,
+    guardian_one_name,
+    guardian_one_email,
+    guardian_one_phone,
+    guardian_two_name,
+    guardian_two_email,
+    guardian_two_phone,
+    middleschool_absences,
+    highschool_absences,
+    highschool_suspensions,
+    total_credits_earned,
+    date_modified,
+    user_modified
+) VALUES (
+    16057,
+    2,
+    "Stuart",
+    "Obrien"
+    2003-5-15,
+    2016-3-26,
+    5768941272,
+    "SObrien09@gmail.com",
+    "Perry",
+    "PObrien62@gmail.com",
+    6529309841
+    "Jennifer",
+    "JObrien12@gmail.com"
+    3596719542,
+    6,
+    4,
+    4,
+    3.90,
+    32,
+    Now(),
+    1
+)
+
+INSERT INTO cohort (
+    student_id,
+    cohort_id,
+    student_first_name,
+    student_last_name,
+    student_dob,
+    student_start_date,
+    student_phone,
+    student_email,
+    guardian_one_name,
+    guardian_one_email,
+    guardian_one_phone,
+    guardian_two_name,
+    guardian_two_email,
+    guardian_two_phone,
+    middleschool_absences,
+    highschool_absences,
+    highschool_suspensions,
+    total_credits_earned,
+    date_modified,
+    user_modified
+) VALUES (
+    75840,
+    2,
+    "Nellie",
+    "Hawkins",
+    2002-2-14,
+    2015-7-24,
+    1920351272,
+    "NHawkins20@gmail.com",
+    "Spencer",
+    "SHawkins53@gmail.com",
+    6520859087
+    "Sheryl",
+    "SHawkins13@gmail.com"
+    2942842301,
+    6,
+    3,
+    5,
+    3.86,
+    27,
+    Now(),
+    1
+)
+
+-- TODO
+-- School 3
+INSERT INTO cohort (
+    student_id,
+    cohort_id,
+    student_first_name,
+    student_last_name,
+    student_dob,
+    student_start_date,
+    student_phone,
+    student_email,
+    guardian_one_name,
+    guardian_one_email,
+    guardian_one_phone,
+    guardian_two_name,
+    guardian_two_email,
+    guardian_two_phone,
+    middleschool_absences,
+    highschool_absences,
+    highschool_suspensions,
+    total_credits_earned,
+    date_modified,
+    user_modified
+) VALUES (
+    96047,
+    3,
+    "Lindsay",
+    "Morrison",
+    2000-11-11,
+    2015-11-11,
+    5018351272,
+    "LMorrison42@gmail.com",
+    "Lucas",
+    "LMorrison22@gmail.com",
+    6529375241
+    "Holly",
+    "HMorrison65@gmail.com"
+    3591712902,
+    5,
+    2,
+    1,
+    3.78,
+    22,
+    Now(),
+    1
+)
+
+INSERT INTO cohort (
+    student_id,
+    cohort_id,
+    student_first_name,
+    student_last_name,
+    student_dob,
+    student_start_date,
+    student_phone,
+    student_email,
+    guardian_one_name,
+    guardian_one_email,
+    guardian_one_phone,
+    guardian_two_name,
+    guardian_two_email,
+    guardian_two_phone,
+    middleschool_absences,
+    highschool_absences,
+    highschool_suspensions,
+    total_credits_earned,
+    date_modified,
+    user_modified
+) VALUES (
+    96057,
+    3,
+    "Ray",
+    "Massey"
+    2001-6-21,
+    2015-6-11,
+    5018841272,
+    "RMassey85@gmail.com",
+    "Gary",
+    "GMassey39@gmail.com",
+    6529309241
+    "Evelyn",
+    "EMassey45@gmail.com"
+    3596713902,
+    4,
+    7,
+    2,
+    3.76,
+    28,
+    Now(),
+    1
+)
+
+INSERT INTO cohort (
+    student_id,
+    cohort_id,
+    student_first_name,
+    student_last_name,
+    student_dob,
+    student_start_date,
+    student_phone,
+    student_email,
+    guardian_one_name,
+    guardian_one_email,
+    guardian_one_phone,
+    guardian_two_name,
+    guardian_two_email,
+    guardian_two_phone,
+    middleschool_absences,
+    highschool_absences,
+    highschool_suspensions,
+    total_credits_earned,
+    date_modified,
+    user_modified
+) VALUES (
+    95047,
+    3,
     "Lisa",
     "Hall",
     2002-3-06,
