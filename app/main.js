d8b5ac42d6e18090ab4a73cc79d46a8a9f0995a3
Benjamin Geier <Benjamin Geier>
2017-03-03 16:00:46 -0500

    Updated side bar to include all school data

--- app/main.js
+++ app/main.js
@@ -61,7 +61,7 @@ app.get('/callback',
   
   //Student routes
   app.get('/students',ensureLog, function(req, res) {
-	connection.query("SELECT * FROM Student", function(err, rows){
+	connection.query("SELECT * FROM student", function(err, rows){
         res.render('students', { students: rows});
     });
 });
@@ -73,7 +73,7 @@ app.get('/callback',
   app.get('/students/:id/profile',ensureLog, function(req, res) {
   //console.log(req);
   console.log(req.params);
-  var query = "SELECT * FROM Student WHERE id=" + req.params.id;
+  var query = "SELECT * FROM student WHERE id=" + req.params.id;
   connection.query(query, function(err, rows){
     console.log(rows[0]);
     rows[0].dob = rows[0].dob.toDateString(); //properly set date.
@@ -88,7 +88,7 @@ app.get('/callback',
   app.post('/addStudent',ensureLog, function(req, res) {
 	res.redirect('/students');
      console.log(req.body);
-     stmt = 'INSERT INTO Student(first_name,last_name,dob,startdate,email,parentone_name,parentone_num,parentone_email,parenttwo_name,parenttwo_num,parenttwo_email) VALUES (?,?,?,?,?,?,?,?,?,?,?);';
+     stmt = 'INSERT INTO student(first_name,last_name,dob,startdate,email,parentone_name,parentone_num,parentone_email,parenttwo_name,parenttwo_num,parenttwo_email) VALUES (?,?,?,?,?,?,?,?,?,?,?);';
      connection.query(stmt,[req.body.first_name,req.body.last_name,new Date(req.body.dob),Date.now(),req.body.email,req.body.parentone_name,req.body.parentone_num,req.body.parentone_email,req.body.parenttwo_name,req.body.parenttwo_num,req.body.parenttwo_email], function(err, rows){ 
       console.log(err);
     });
@@ -97,13 +97,9 @@ app.get('/callback',
   
   //School routes
 app.get('/schools', function(req, res) {
-  connection.query("SELECT * FROM School", function(err, rows){
+  connection.query("SELECT * FROM school", function(err, rows){
         res.render('schools', { schools: rows});
     });
-});
-
-app.delete('/schools', function(req, res) {
-  console.log("Deleted");
 });
 
 app.get('/addSchool', function(req, res) {
@@ -113,8 +109,8 @@ app.get('/addSchool', function(req, res) {
 app.post('/addSchool', function(req, res) {
     res.redirect('/schools');
     console.log(req.body);
-    stmt = 'INSERT INTO School(school_name,principle,principle_phone,school_address,school_phone) VALUES (?,?,?,?,?);';
-    connection.query(stmt,[req.body.schoolName,req.body.principleName,req.body.principlePhone,req.body.schoolAddress,req.body.schoolNumber], function(err, rows) {
+    stmt = 'INSERT INTO school(school_name,school_address,school_phone,principal_name,principal_phone,principal_email,assistant_principal_name,assistant_principal_phone,assistant_principal_email,cohort_coach_name,cohort_coach_phone,cohort_coach_email,ngm_fellow_name,ngm_fellow_phone,ngm_fellow_email,counselor_name,counselor_phone,counselor_email,social_worker_name,social_worker_phone,social_worker_email,data_liason_name,data_liason_phone,data_liason_email,cis_coordinator_name,cis_coordinator_phone,cis_coordinator_email,college_coach_name,college_coach_phone,college_coach_email) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
+    connection.query(stmt,[req.body.school_name,req.body.school_address,req.body.school_phone,req.body.principal_name,req.body.principal_phone,req.body.principal_email,req.body.assistant_principal_name,req.body.assistant_principal_phone,req.body.assistant_principal_email,req.body.cohort_coach_name,req.body.cohort_coach_phone,req.body.cohort_coach_email,req.body.ngm_fellow_name,req.body.ngm_fellow_phone,req.body.ngm_fellow_email,req.body.counselor_name,req.body.counselor_phone,req.body.counselor_email,req.body.social_worker_name,req.body.social_worker_phone,req.body.social_worker_email,req.body.data_liason_name,req.body.data_liason_phone,req.body.data_liason_email,req.body.cis_coordinator_name,req.body.cis_coordinator_phone,req.body.cis_coordinator_email,req.body.college_coach_name,req.body.college_coach_phone,req.body.college_coach_email], function(err, rows) {
       console.log(err);
     });
 });
@@ -122,14 +118,14 @@ app.post('/addSchool', function(req, res) {
 app.get('/schools/:id/delete', function(req, res) {
   console.log(req.params);
   res.redirect('/schools');
-  connection.query("DELETE FROM School WHERE school_id=" + req.params.id, function(err, rows) {
+  connection.query("DELETE FROM school WHERE school_id=" + req.params.id, function(err, rows) {
     console.log(err);
   });
 });
 
 app.get('/schools/:id/profile', function(req, res) {
   console.log(req.params);
-  var query = "SELECT * FROM School WHERE school_id=" + req.params.id;
+  var query = "SELECT * FROM school WHERE school_id=" + req.params.id;
   connection.query(query, function(err, rows) {
     console.log(rows[0]);
     res.render('schoolDetail', {school: rows[0]});
