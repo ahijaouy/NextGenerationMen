80e99c93fb3526921de1ac1beea89cbaa474baab
Benjamin Geier <Benjamin Geier>
2017-03-09 17:09:53 -0500

    Update student now functional

--- app/main.js
+++ app/main.js
@@ -109,7 +109,6 @@ module.exports = function(app, passport) {
 
 app.post('/addSchool', function(req, res) {
     res.redirect('/schools');
-    console.log(req.body);
     stmt = 'INSERT INTO school(school_name,school_address,school_phone,principal_name,principal_phone,principal_email,assistant_principal_name,assistant_principal_phone,assistant_principal_email,cohort_coach_name,cohort_coach_phone,cohort_coach_email,ngm_fellow_name,ngm_fellow_phone,ngm_fellow_email,counselor_name,counselor_phone,counselor_email,social_worker_name,social_worker_phone,social_worker_email,data_liason_name,data_liason_phone,data_liason_email,cis_coordinator_name,cis_coordinator_phone,cis_coordinator_email,college_coach_name,college_coach_phone,college_coach_email) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);';
     connection.query(stmt,[req.body.school_name,req.body.school_address,req.body.school_phone,req.body.principal_name,req.body.principal_phone,req.body.principal_email,req.body.assistant_principal_name,req.body.assistant_principal_phone,req.body.assistant_principal_email,req.body.cohort_coach_name,req.body.cohort_coach_phone,req.body.cohort_coach_email,req.body.ngm_fellow_name,req.body.ngm_fellow_phone,req.body.ngm_fellow_email,req.body.counselor_name,req.body.counselor_phone,req.body.counselor_email,req.body.social_worker_name,req.body.social_worker_phone,req.body.social_worker_email,req.body.data_liason_name,req.body.data_liason_phone,req.body.data_liason_email,req.body.cis_coordinator_name,req.body.cis_coordinator_phone,req.body.cis_coordinator_email,req.body.college_coach_name,req.body.college_coach_phone,req.body.college_coach_email], function(err, rows) {
       console.log(err);
@@ -117,24 +116,32 @@ app.post('/addSchool', function(req, res) {
 });
 
 app.get('/schools/:id/delete', function(req, res) {
-  console.log(req.params);
-  res.redirect('/schools');
   connection.query("DELETE FROM school WHERE school_id=" + req.params.id, function(err, rows) {
+    res.redirect('/schools');
     console.log(err);
   });
 });
 
 app.get('/schools/:id/profile', function(req, res) {
-  console.log(req.params);
   var query = "SELECT * FROM school WHERE school_id=" + req.params.id;
   connection.query(query, function(err, rows) {
-    console.log(rows[0]);
     res.render('schoolDetail', {school: rows[0]});
   });
 });
 
 app.get('/schools/:id/edit', function(req, res) {
-  console.log(req.params);
+  var query = "SELECT * FROM school WHERE school_id=" + req.params.id;
+  connection.query(query, function(err, rows) {
+    res.render('editSchool', {school: rows[0]});
+  })
+});
+
+app.post('/schools/:id/edit', function(req, res) {
+  var query = "UPDATE school SET school_name = ?, school_address = ?, school_phone = ?, principal_name = ?, principal_phone = ?, principal_email = ?, assistant_principal_name = ?, assistant_principal_phone = ?, assistant_principal_email = ?, cohort_coach_name = ?, cohort_coach_phone = ?, cohort_coach_email = ?, ngm_fellow_name = ?, ngm_fellow_phone = ?, ngm_fellow_email = ?, counselor_name = ?, counselor_phone = ?, counselor_email = ?, social_worker_name = ?, social_worker_phone = ?, social_worker_email = ?, data_liason_name = ?, data_liason_phone = ?, data_liason_email = ?, cis_coordinator_name = ?, cis_coordinator_phone = ?, cis_coordinator_email = ?, college_coach_name = ?, college_coach_phone = ?, college_coach_email = ? WHERE school_id = " + req.params.id;
+  connection.query(query, [req.body.school_name,req.body.school_address,req.body.school_phone,req.body.principal_name,req.body.principal_phone,req.body.principal_email,req.body.assistant_principal_name,req.body.assistant_principal_phone,req.body.assistant_principal_email,req.body.cohort_coach_name,req.body.cohort_coach_phone,req.body.cohort_coach_email,req.body.ngm_fellow_name,req.body.ngm_fellow_phone,req.body.ngm_fellow_email,req.body.counselor_name,req.body.counselor_phone,req.body.counselor_email,req.body.social_worker_name,req.body.social_worker_phone,req.body.social_worker_email,req.body.data_liason_name,req.body.data_liason_phone,req.body.data_liason_email,req.body.cis_coordinator_name,req.body.cis_coordinator_phone,req.body.cis_coordinator_email,req.body.college_coach_name,req.body.college_coach_phone,req.body.college_coach_email], function(err, rows) {
+    console.log(err);
+    res.redirect('/schools/' + req.params.id + '/profile');
+  });
 });
   
   // //Partner routes
