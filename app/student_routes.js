3e8e0866d36475f37c96b775cc22c88399c6e93c
Oberon <ahijaouy@gatech.edu>
2017-04-10 11:51:19 -0400

    add attendance_record to student profile

--- app/student_routes.js
+++ app/student_routes.js
@@ -29,20 +29,41 @@ router.route('/:id/profile')
       
       var academicRecord = "SELECT * FROM semester_record WHERE student_id=" + req.params.id + ";";
       connection.query(academicRecord, function(err, recordRows) {
-        if (recordRows === undefined) {
-          res.render('profile', {student: rows[0], record: []});
-        } else {
-          res.render('profile', { student: rows[0], record: recordRows});
-        }
+        var attendanceRecord = "SELECT * FROM attendance_record WHERE student_id=" + req.params.id + ";";
+        connection.query(attendanceRecord, function(err, attendanceRows) {
+          if (recordRows === undefined && attendanceRows === undefined) {
+            res.render('profile', {student: rows[0], record: [], attendance: []});
+          } else if (recordRows === undefined && attendanceRows != undefined){
+            res.render('profile', { student: rows[0], record: [], attendance: attendanceRows});
+          } else if (recordRows != undefined && attendanceRows === undefined) {
+            res.render('profile', { student: rows[0], record: recordRows, attendance: []});
+          } else {
+            res.render('profile', { student: rows[0], record: recordRows, attendance: attendanceRows});
+          }
+        });
       });
     });
-  })
+  });
+
+//Post Semester Record
+router.route('/:id/profile/academic')
+  .all(authenticate, function (req, res, next) {next();})
   .post(function (req, res, next) {
     res.redirect('/students/' + req.params.id +'/profile');
-    var stmt = 'INSERT INTO semester_record(student_id, number_as, number_bs, number_cs, number_ds, semester_number, grade, semester_gpa, semester_credits) VALUES (?,?,?,?,?,?,?,?,?);';
+    var stmt = 'INSERT INTO semester_record(student_id, number_as, number_bs, number_cs, number_ds, semester_number, grade, semester_gpa, semester_credits, date_modified, user_modified) VALUES (?,?,?,?,?,?,?,?,?,?,?);';
     var gpa = ((parseInt(req.body.number_as) * 4) + (parseInt(req.body.number_bs) * 3) + (parseInt(req.body.number_cs) * 2)) / (parseInt(req.body.number_as) + parseInt(req.body.number_bs) + parseInt(req.body.number_cs) + parseInt(req.body.number_ds));
-    console.log(req.body);
-    connection.query(stmt, [req.params.id, req.body.number_as, req.body.number_bs, req.body.number_cs, req.body.number_ds, req.body.semester_number, req.body.grade, gpa, req.body.semester_credits], function(err, rows) {
+    connection.query(stmt, [req.params.id, req.body.number_as, req.body.number_bs, req.body.number_cs, req.body.number_ds, req.body.semester_number, req.body.grade, gpa, req.body.semester_credits, new Date(Date.now()), "Username"], function(err, rows) {
+      if (err) {console.log(err)}
+    });
+  });
+
+//Post Attendance Record
+router.route('/:id/profile/attendance')
+  .all(authenticate, function (req, res, next) {next();})
+  .post(function (req, res, next) {
+    res.redirect('/students/' + req.params.id +'/profile');
+    var stmt = 'INSERT INTO attendance_record(student_id, classes_missed, days_missed, suspensions, semester_number, grade, date_modified, user_modified) VALUES (?,?,?,?,?,?,?,?);';
+    connection.query(stmt, [req.params.id, req.body.classes_missed, req.body.days_missed, req.body.suspensions, req.body.semester_number, req.body.grade, new Date(Date.now()), "Username"], function(err, rows) {
       if (err) {console.log(err)}
     });
   });
@@ -79,7 +100,7 @@ router.route('/:id/edit')
   })
   .post(function(req, res, next) {
     var query = "UPDATE student SET   student_first_name = ?, student_last_name = ?, student_dob = ?, student_gender = ?, student_phone = ?, student_email = ?, guardian_one_name = ?, guardian_one_email = ?, guardian_one_phone = ?, guardian_two_name = ?, guardian_two_email = ?, guardian_two_phone = ?, middleschool_suspensions = ?, highschool_absences = ?, highschool_suspensions = ?, cumulative_gpa = ?, total_credits_earned = ?, date_modified = ?, user_modified = ? WHERE student_id = ?;"
-    connection.query(query, [req.body.student_first_name,req.body.student_last_name,req.body.student_dob,req.body.gender,req.body.student_phone,req.body.student_email,req.body.guardian_one_name,req.body.guardian_one_email,req.body.guardian_one_phone,req.body.guardian_two_name,req.body.guardian_two_email,req.body.guardian_two_phone,req.body.middleschool_suspensions,req.body.highschool_absence,req.body.highschool_suspensions,req.body.cumulative_gpa,req.body.total_credits_earned,Date.now(),req.body.user_modified, req.params.id], function(err, rows) {
+    connection.query(query, [req.body.student_first_name,req.body.student_last_name,req.body.student_dob,req.body.gender,req.body.student_phone,req.body.student_email,req.body.guardian_one_name,req.body.guardian_one_email,req.body.guardian_one_phone,req.body.guardian_two_name,req.body.guardian_two_email,req.body.guardian_two_phone,req.body.middleschool_suspensions,req.body.highschool_absence,req.body.highschool_suspensions,req.body.cumulative_gpa,req.body.total_credits_earned,new Date(Date.now()),req.body.user_modified, req.params.id], function(err, rows) {
       if (err) {console.log(err);}
       res.redirect('/students/' + req.params.id + '/profile'); 
     });
